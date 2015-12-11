9967e804e5d7d095b5c09b334a241d0b4b1c9f14
hocuspocus2015 <alicesuckling@live.co.uk>
2015-12-11 19:20:12 +0000

    update address_bloc.rb

---
+++ models/address_book.rb
@@ -0,0 +1,34 @@
+require_relative 'entry'
+
+class AddressBook
+ attr_accessor :entries
+  def initialize
+     @entries = []
+  end
+  
+  def add_entry(name, phone_number, email)
+      
+    index = 0
+    @entries.each do |entry|
+         
+     if name < entry.name
+        break
+     end
+       
+     index += 1
+    end
+     
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+    
+    def remove_entry(name, phone_number, email)
+        remove_entry = nil
+        @entries.each do |entry|
+            if name == entry.name || phone_number == entry.phone_number || email == entry.email
+                remove_entry = Entry
+            end
+        end
+        @entries.delete(remove_entry)
+    end
+  
+end
